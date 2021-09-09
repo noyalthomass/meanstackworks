5828e5986fcffc385020d915b755b45c3b73787d
Noyal Thomas <noyallthomas@gmail.com>
2021-09-09 11:29:54 +0530

    msg

---
+++ javascript_works/script.js
@@ -0,0 +1,24 @@
+// java script programming
+
+//to print a message in console
+//print()----------------------python
+//printf()---------------------c
+//count>>----------------------c++
+//System.out.printIn()---------java
+//console.log()----------------javascript
+//identifiers-name is called identifier
+
+//int ag=22 age-variable name= lower_case,noun
+
+//fuction displayValues() display-fuction name
+//print_person()-snake notation-python
+//printPerson()-camel notation-javascript-2nd word starting with upper case in fuction
+
+//class Person{} person-classname
+//classname-PersonDetails-p and d uppercase in class
+
+
+//var,let,const
+//hai i amm,age years old
+
+console.log("Hello world")

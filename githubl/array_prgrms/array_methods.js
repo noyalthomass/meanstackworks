2f892a209fb57a8c895ed24ec6a6fc716d7804fa
noyalthomass <90236833+noyalthomass@users.noreply.github.com>
2021-09-24 11:56:11 +0530

    Add files via upload

---
+++ githubl/array_prgrms/array_methods.js
@@ -0,0 +1,30 @@
+//map
+//filter
+//reduce
+//sort
+//some
+//find
+//forEach
+
+var arr=[2,3,4,5,6]
+
+//filter -----array-yil condition apply aakunnidel filter use cheyyam
+//find even num from array
+//find odd
+
+//map---condition illa,input-nu correspond output generate akkunnu
+//print squre if each no in array
+//using for loop
+//for(let num of arr){
+//    console.log(num**2);
+//}
+
+//using map
+//var squres=arr.map((num)=>num**2)
+//console.log(squres);
+
+
+
+//pint cubes
+//var cubes=arr.map((num)=>num**3)
+//console.log(cubes);
---
+++ githubl/array_prgrms/arraydemo.js
@@ -0,0 +1,44 @@
+//datastructures
+// for organizing data-0
+
+//define?----[]
+//is it possible to store same and different types of object/data
+//size not fixed
+//insertion order preserved 
+//fectching array elements index
+//array index starting from 0th index
+//              0     1    2      3     4     5
+var expenses=[10000,12000,22000,70000,80000,90000]
+console.log(expenses[0]);
+
+//using loop
+//for(let i=0;i<expenses.length;i++){
+ //   console.log(expenses[i]);
+//}
+
+
+
+
+//for(let amount of expenses){
+ //   console.log(amount);
+//}
+
+//print total
+
+//var total=0;
+//for (let amount of expenses){
+//    total+=amount;
+//}
+//console.log(total);
+
+//value add akkan-inserting an element  to end of the array
+//expenses.push(90090)
+//console.log(expenses);
+
+//value update akkan
+//expenses[0]=expenses[0]+2000;
+//console.log(expenses);
+
+//end element remove akkan pop()
+//expenses.pop()
+//console.log(expenses);
